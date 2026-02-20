474db81e5e35c077a41f2089c6f609fc733681c5
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2026-02-20 13:00:42 +0530

    feat: implement user model and schema for user creation

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from collections.abc import AsyncGenerator
 from enum import StrEnum
 import uuid
 from typing import Optional
-from sqlalchemy import Text, String, DateTime, ForeignKey, UUID, Enum
+from sqlalchemy import Text, String, DateTime, ForeignKey, UUID, Enum, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio  import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
@@ -16,6 +16,20 @@ DATABASE_URL="sqlite+aiosqlite:///./test.db"
 class Base(DeclarativeBase):
     pass
 
+class User(Base):
+    __tablename__="users"
+
+    id: Mapped[uuid.UUID]=mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    username: Mapped[str]=mapped_column(String,nullable=False)
+    email:Mapped[str]=mapped_column(String,nullable=False)
+    hashed_password:Mapped[str]=mapped_column(String, nullable=False)
+
+    first_name: Mapped[str]=mapped_column(String, nullable=False)
+    last_name: Mapped[Optional[str]]=mapped_column(String)
+
+    is_active:Mapped[Optional[bool]]=mapped_column(Boolean, default=True)
+
+
 class FileCategory(StrEnum):
     IMAGE="image"
     VIDEO="video"
@@ -24,7 +38,7 @@ class Post(Base):
     __tablename__="posts"
 
     id: Mapped[uuid.UUID] =mapped_column(UUID, primary_key=True, default=uuid.uuid4)
-    user_id: Mapped[uuid.UUID]=mapped_column(UUID, ForeignKey("user.id"), nullable=False)
+    user_id: Mapped[uuid.UUID]=mapped_column(UUID, ForeignKey("users.id"), nullable=False)
 
     caption: Mapped[Optional[str]]=mapped_column(Text)
     url: Mapped[str]=mapped_column(Text, nullable=False)
