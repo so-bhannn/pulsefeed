99f6d683d61dd72d5183042a05065560acc81067
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2026-02-21 16:49:41 +0530

    refactor: reorganize CreateUser schema fields for clarity

--- app/schema.py
+++ app/schema.py
@@ -1,21 +1,7 @@
-from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
-from dotenv import loadenv
-import os
-
-
-from app.db import User
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
-
-
 class CreateUser(BaseModel):
-    first_name:str
-    last_name:str | None=None
     username:str
-
     email:str
     password:str
-    
-    is_active:bool
+    first_name:str
+    last_name:str | None=None
